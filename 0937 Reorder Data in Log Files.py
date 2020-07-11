f87a9f119e356741549800e96ec05dbd2ddb8be8
Seung Won [Tom] Lee <seungwon.lee@alumni.ubc.ca>
2020-07-11 16:45:38 +0900

    Create 0937 Reorder Data in Log Files.py

---
+++ 0937 Reorder Data in Log Files.py
@@ -0,0 +1,26 @@
+from heapq import heappush, heappop
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letterHeap = []
+        digitResult = []
+        
+        for val in logs:
+            head, tail = val.split(" ", 1)
+            # doesn't work with "id a"
+            # first, rest = tail.split(" ", 1)
+            words = tail.split(" ", 1)
+            if words[0].isdigit():
+                digitResult.append(val)
+            else:
+                heappush(letterHeap, (tail, val))
+                
+        result = [heappop(letterHeap)[1] for i in range(len(letterHeap))]
+        result += digitResult
+        return result
+        
+        
+        
+                
+                
+            
