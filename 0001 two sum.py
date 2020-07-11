a3c87981a124bf67b2ea946c95ca1d3af9f7eaa7
Seung Won [Tom] Lee <seungwon.lee@alumni.ubc.ca>
2020-07-11 16:08:05 +0900

    Delete 0001 two sum.py

--- 0001 two sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        store = {}
-        for idx, val in enumerate(nums):
-            if (target - val) in store:
-                return (store[target - val], idx)
-            store[val] = idx
-        
