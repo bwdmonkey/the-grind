c10f6a07f18277d1edad40e797a665c099c1b871
Seung Won [Tom] Lee <seungwon.lee@alumni.ubc.ca>
2020-07-19 21:33:07 +0900

    Create 0016 Three Sum Closest.py.py

---
+++ 0016 Three Sum Closest.py.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = None
+        nums.sort()
+        
+        for idx, val in enumerate(nums):
+            l, r = idx + 1, len(nums) - 1
+            
+            while l < r:
+                curr_sum = val + nums[l] + nums[r]
+                
+                if diff != None:
+                    if abs(target - curr_sum) < abs(diff):
+                        diff = target - curr_sum
+                else:
+                    diff = target - curr_sum
+                
+                if target > curr_sum:
+                    l += 1
+                else:
+                    r -= 1
+        
+        return target - diff
