92a475293cc288348e32528f538f3fdf401b5b69
Seung Won [Tom] Lee <seungwon.lee@alumni.ubc.ca>
2020-08-02 15:56:01 +0900

    Create 0482 License Key Formatting.py

---
+++ 0482 License Key Formatting.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/license-key-formatting/
+
+# Version 1
+class SolutionOne:
+    def licenseKeyFormatting(self, S: str, K: int) -> str:
+        part, parts = '', []
+        
+        for i in range(len(S))[::-1]:
+            if S[i] == "-":
+                continue
+            
+            part = f'{S[i].upper()}{part}'
+            
+            if len(part) == K:
+                parts = [part] + parts
+                part = ''
+        
+        if len(part) > 0:
+            parts = [part] + parts
+        
+        # print(parts)
+        return '-'.join(parts)
+        
+# Version 2
+class SolutionTwo:
+    def licenseKeyFormatting(self, S: str, K: int) -> str:
+        reverse = S.replace('-', '').upper()[::-1]
+        reverse_parts = [reverse[i:i + K][::-1] for i in range(0, len(reverse), K)]
+        return '-'.join(reverse_parts[::-1])
